bfbe4996ebf7a5bdd151f06e0fa14b62b634facb
Samarth Goyal <samarth.goyal1999@gmail.com>
2025-02-02 15:30:32 +0530

    Test Code and Setup Code fix

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="csvgres",
-    packages=find_packages(),
     version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        line.strip()
+        for line in open("requirements.txt")
+        if not line.startswith("#") and line.strip()
+    ],
 ) 
