cb65bc5a586b895f3c7baa1ce3832375499c7322
Samarth Goyal <samarth.goyal1999@gmail.com>
2025-03-23 01:14:54 +0530

    fix: annd code for float_ deprecated

--- transformer/operations/data_ops.py
+++ transformer/operations/data_ops.py
@@ -448,7 +448,7 @@ class DataOperations:
             return [self._convert_to_json_serializable(v) for v in obj]
         elif isinstance(obj, (np.int_, np.intc, np.intp, np.int8, np.int16, np.int32, np.int64)):
             return int(obj)
-        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+        elif isinstance(obj, (np.float16, np.float32, np.float64)):
             return float(obj)
         elif isinstance(obj, np.bool_):
             return bool(obj)
